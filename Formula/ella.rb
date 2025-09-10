1d806b7ce4a996a76452c712f01a8abbdd0267da
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-09-10 23:59:06 +0330

    bot: update to version v0.0.7

--- Formula/ella.rb
+++ Formula/ella.rb
@@ -1,33 +1,32 @@
 class Ella < Formula
   desc "A process manager for running and managing services"
   homepage "https://github.com/thekhanj/ella"
-  version "0.0.6"
+  version "0.0.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/thekhanj/ella/releases/download/v#{version}/ella_v#{version}_darwin_arm64.tar.gz"
-      sha256 "14bbca6c8e6fb56c56bec7067ee3c15b9f111d71d23d985442fb85a953cf611b"
+      sha256 "d9cd899dd3f50a9905153cab54bd3e579bfc8fa9ecdb2258c0cdbb885c9fbcf0"
     else
       url "https://github.com/thekhanj/ella/releases/download/v#{version}/ella_v#{version}_darwin_amd64.tar.gz"
-      sha256 "7d3bfbf70441eb2f12698d7e49869f6b5343470f796e90e9c80bc7c07ec5ddb0"
+      sha256 "8745003e139676ebfb14c936130df708ddd72d53eda7d64338433fecffbe586d"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/thekhanj/ella/releases/download/v#{version}/ella_v#{version}_linux_arm64.tar.gz"
-      sha256 "e6e1649f60989606fbc0b50e48f1ba673ed72aef39688279a324713aeb6b6000"
+      sha256 "d72cdd80e51d17a7e73e036336fb09ff42ab98ee6a6a4d87e0642f92cf30e6ea"
     else
       url "https://github.com/thekhanj/ella/releases/download/v#{version}/ella_v#{version}_linux_amd64.tar.gz"
-      sha256 "1e32d7224aa1d2d924b42b5f319392ea2dffffe4dd83a9ec7ec86596cc43c1c2"
+      sha256 "e0b50898c8dc57cadcfae6f09355e3895ea9f6c66a1129833c1a44b5802d7744"
     end
   end
 
   def install
     bin.install "ella"
     doc.install "fsm/service.png"
-    doc.install "fsm/service-deactivating.png"
     man1.install "man/ella.1.gz"
     bash_completion.install "completion.sh"
   end
